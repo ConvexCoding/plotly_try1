19e1798c6f9d8884a41488d7d2c283be416a75ba
ConvexCoding <64615930+glherrit@users.noreply.github.com>
2022-03-24 10:52:53 -0400

    weak attempt at plotly

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+
+<svelte:head>
+	<script src="plotly-8.5.1.js"></script>
+</svelte:head>
+
+import * from Plotly;
+
+<script>
+	import { onMount } from 'svelte';
+	  
+	  let headerText;
+  
+	export let plotHeader = '';
+  
+	export let data = [{
+	  x: [0, 10, 20, 30, 40],
+	  y: [0, 2, 6, 14, 30],
+	  type: 'scatter'
+	}];
+  
+	onMount(() => {
+		  headerText = 'On Mount Called !';
+		  let plotDiv = document.getElementById('plotDiv');				
+		  let Plot = new Plotly.newPlot(plotDiv, data, {}, {showSendToCloud:true}); 
+	});
+			  
+  </script>
+  
+  <h3>{headerText}</h3>
+  <div id="plotly">
+	<div>
+	  <h1>{plotHeader}</h1>
+	</div>
+	<div id="plotDiv"><!-- Plotly chart will be drawn inside this DIV --></div>
+  </div>
