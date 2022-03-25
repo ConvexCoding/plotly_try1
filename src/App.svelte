4326b101360636de4b579634b11c4cf47fa5c857
ConvexCoding <64615930+glherrit@users.noreply.github.com>
2022-03-25 00:24:53 -0400

    a working example of plotly line plot, fairly simple

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,47 @@
-
 <svelte:head>
-	<script src="plotly-8.5.1.js"></script>
+  <script src="https://cdn.plot.ly/plotly-latest.min.js" type="text/javascript" on:load={plotlyLoaded}></script>
 </svelte:head>
 
-import * from Plotly;
-
 <script>
-	import { onMount } from 'svelte';
-	  
-	  let headerText;
-  
-	export let plotHeader = '';
-  
-	export let data = [{
-	  x: [0, 10, 20, 30, 40],
-	  y: [0, 2, 6, 14, 30],
-	  type: 'scatter'
-	}];
-  
-	onMount(() => {
-		  headerText = 'On Mount Called !';
-		  let plotDiv = document.getElementById('plotDiv');				
-		  let Plot = new Plotly.newPlot(plotDiv, data, {}, {showSendToCloud:true}); 
-	});
-			  
-  </script>
-  
-  <h3>{headerText}</h3>
-  <div id="plotly">
-	<div>
-	  <h1>{plotHeader}</h1>
-	</div>
-	<div id="plotDiv"><!-- Plotly chart will be drawn inside this DIV --></div>
+  import { onMount } from 'svelte';
+	let plotlyValid = false;
+	let mounted = false;
+	let headerText;
+	let plotDiv;
+
+  let plotHeader = '';
+
+  const data = [{
+    x: [0, 10, 20, 30, 40, 50],
+    y: [0, 5, 10, 30, 60, 90],
+    type: 'line'
+  }];
+	
+	const loadPlots = () => {
+		let Plot = new Plotly.newPlot(plotDiv, data, {}, {showSendToCloud:true}); 
+	}
+	
+	const plotlyLoaded = () => {
+		plotlyValid = true;
+		if(mounted){
+			loadPlots();
+		}
+	}
+
+  onMount(() => {
+		headerText = 'On Mount Called !';
+		mounted = true;
+		if(plotlyValid){
+			loadPlots();
+		}
+  });
+            
+</script>
+
+<h3>{headerText}</h3>
+<div id="plotly">
+  <div>
+    <h1>{plotHeader}</h1>
   </div>
+  <div id="plotDiv" bind:this={plotDiv}><!-- Plotly chart will be drawn inside this DIV --></div>
+</div>
